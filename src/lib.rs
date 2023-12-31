c41eda93eb39c35dc4be26f934ff7f97100afde1
zzwxh <zzwxh123@gmail.com>
2023-12-31 10:09:01 +0000

    add recv method

--- src/lib.rs
+++ src/lib.rs
@@ -4,29 +4,40 @@ use std::{
     task::{Poll, Waker},
 };
 
-use quiche::{Config, Connection, ConnectionId, Error, Result, SendInfo};
+use quiche::{Config, Connection, ConnectionId, Error, RecvInfo, Result, SendInfo};
 
 pub struct AsyncConn {
     inner: RefCell<Connection>,
-    send: Cell<Option<Waker>>,
+    send_waker: Cell<Option<Waker>>,
 }
 
 impl AsyncConn {
     pub async fn send(&self, out: &mut [u8]) -> Result<(usize, SendInfo)> {
         std::future::poll_fn(|cx| match self.inner.borrow_mut().send(out) {
             Err(Error::Done) => {
-                self.send.replace(Some(cx.waker().clone()));
+                self.send_waker.replace(Some(cx.waker().clone()));
                 Poll::Pending
             }
             v => Poll::Ready(v),
         })
         .await
     }
+
+    pub fn recv(&self, buf: &mut [u8], info: RecvInfo) -> Result<usize> {
+        self.wake_send();
+        self.inner.borrow_mut().recv(buf, info)
+    }
+
+    fn wake_send(&self) {
+        if let Some(waker) = self.send_waker.take() {
+            waker.wake();
+        }
+    }
 }
 
 pub fn connect(server_name: Option<&str>, scid: &ConnectionId, local: SocketAddr, peer: SocketAddr, config: &mut Config) -> Result<AsyncConn> {
     quiche::connect(server_name, scid, local, peer, config).map(|conn| AsyncConn {
         inner: RefCell::new(conn),
-        send: Cell::new(None),
+        send_waker: Cell::new(None),
     })
 }
